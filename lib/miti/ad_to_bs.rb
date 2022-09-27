4051b245854c1f794f0e541a3cb8137ed7d683ab
sanzaymdr <sanzaymanandhar99@gmail.com>
2022-09-27 08:47:44 +0545

    feat: add rdocs documentation for ad_to_bs class

--- lib/miti/ad_to_bs.rb
+++ lib/miti/ad_to_bs.rb
@@ -3,17 +3,40 @@
 module Miti
   # class to handle the algorithm for converting AD to BS
   class AdToBs
+    ##
+    # Initializes a AdToBs object
+    # @param @english_date [Date]
+    #
     def initialize(english_date)
       @english_date = english_date
       @nepali_year = english_date.year + 56
     end
 
+    ##
+    # Converts the @english_date to nepali date
+    # @param to_h [boolean], default set to false
+    #
+    # @return <NepaliDate>, refers to converted nepali date object
+    # @return [Hash], hash consisting (:barsa, :mahina, :gatey) if @params to_h is true
     def convert(to_h: false)
       return NepaliDate.new(barsa: final_nepali_year_month_value[:year], mahina: mahina, gatey: gatey) unless to_h
 
       { barsa: final_nepali_year_month_value[:year], mahina: mahina, gatey: gatey }
     end
 
+    ##
+    # Calculates the nepalese year and month respectively
+    # determines the difference of nepali_nth_day and total_days_in_nepali_year
+    #
+    # if the difference is positive
+    # - month -> @english_date's month - 4 = nepali_month
+    # - year -> @english_date's year + 1 = nepali year
+    #
+    # if negative
+    # - month -> @english_date's month + 8 = nepali_month
+    # - year -> @english_date's year = nepali year
+    #
+    # @return [Hash],which includes (:year,:month) *[converted to nepali]*
     def final_nepali_year_month_value
       @final_nepali_year_month_value = if (nepali_nth_day - total_days_in_nepali_year).positive?
                                          { year: nepali_year + 1, month: english_date.month - 4 }
@@ -57,6 +80,13 @@ module Miti
       end
     end
 
+    ##
+    # Calculates the remaining days in a specific year
+    # by retrieving the modulo of nepali_nth_day to total_days_in_nepali_year
+    # - nepali_nth_day -> number of days from 1st Jan
+    # - total_days_in_nepali_year -> number of days in specific nepali year
+    #
+    # @return [Integer], refers to number of days remaining in nepali year
     def remaining_days_in_nepali_year
       remaining_days = nepali_nth_day % total_days_in_nepali_year
       if remaining_days.zero?
@@ -66,25 +96,43 @@ module Miti
       end
     end
 
+    ##
+    # Calculates the total number of days in a specific year
+    #
+    # @return [Integer], refers to the total number of days
     def total_days_in_nepali_year
       @total_days_in_nepali_year ||= year_data[nepali_year].sum
     end
 
+    ##
+    # Calculates the number of days in nepali year as on from Jan 1st
+    #
+    # @return [Integer], refers to number of days
     def nepali_nth_day
       @nepali_nth_day = english_date.yday + nepali_nth_day_for_english_new_year - 1
     end
 
+    ##
+    # Determines the number of days from Jan 1st, for leap years
     def nepali_nth_day_for_english_new_year
       return 264 if year_after_leap_year?
 
       263
     end
 
+    ##
+    # Checks the previous year of @english_date from params is leap year or not?
+    #
+    # @return [Boolean], refers previous year is whether leap year or not
     def year_after_leap_year?
       year_before = english_date.year - 1
       (year_before % 400).zero? || (year_before % 100 != 0 && (year_before % 4).zero?)
     end
 
+    ##
+    # Calls DateData class to retrieve the nepali date data consisting total no of days, month
+    #
+    # @return [Hash], refers to hash object consisting the nepali date data
     def year_data
       @year_data = Miti::Data::DateData.year_month_days_hash
     end
