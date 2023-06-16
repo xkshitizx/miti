132c7441c392f18947852b3c1d9eac22f04b973f
sanzay_mdr <sanzaymanandhar99@gmail.com>
2023-06-16 20:29:04 +0545

    feat: handle ConversionUnavailableError for CLI
    * Rescue ConversionUnavailableError when conversion is not available and print the error

--- lib/cli.rb
+++ lib/cli.rb
@@ -10,6 +10,7 @@ module Miti
     def initialize(*args)
       super
       @shell = Thor::Shell::Color.new
+      @output_color = :green
     end
 
     desc "today", "today's nepali miti"
@@ -21,19 +22,25 @@ module Miti
     end
 
     desc "on ENGLISH_DATE", "converts english date to nepali miti"
-    def on(english_date)
+    def to_bs(english_date)
       converted_nepali_miti = Miti.to_bs(english_date)
       output_txt = "[#{converted_nepali_miti}] #{converted_nepali_miti.descriptive}"
-
-      @shell.say(output_txt, :green)
+    rescue ConversionUnavailableError => e
+      output_txt = e
+      @output_color = :red
+    ensure
+      @shell.say(output_txt, @output_color)
     end
 
     desc "to NEPALI_DATE", "converts nepali miti to english date"
-    def to(nepali_date)
+    def to_ad(nepali_date)
       converted_english_date = Miti.to_ad(nepali_date)
       output_txt = "[#{converted_english_date}] #{converted_english_date.strftime('%B %d, %Y %A')}"
-
-      @shell.say(output_txt, :green)
+    rescue ConversionUnavailableError => e
+      output_txt = e
+      @output_color = :red
+    ensure
+      @shell.say(output_txt, @output_color)
     end
 
     no_commands do
