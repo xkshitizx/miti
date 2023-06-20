aed39537598fd45b21e64205a782e868e05d4b78
sanzay_mdr <sanzaymanandhar99@gmail.com>
2023-06-20 09:47:36 +0545

    chore: output AD or BS for respective converted date for clarity

--- lib/cli.rb
+++ lib/cli.rb
@@ -16,7 +16,7 @@ module Miti
     desc "today", "today's nepali miti"
     def today
       current_nepali_miti = Miti.to_bs(Date.today.to_s)
-      output_txt = "[#{current_nepali_miti}] #{current_nepali_miti.descriptive}"
+      output_txt = "[#{current_nepali_miti} BS] #{current_nepali_miti.descriptive}"
 
       @shell.say(output_txt, :green)
     end
@@ -24,7 +24,7 @@ module Miti
     desc "on ENGLISH_DATE", "converts english date to nepali miti"
     def to_bs(english_date)
       converted_nepali_miti = Miti.to_bs(english_date)
-      output_txt = "[#{converted_nepali_miti}] #{converted_nepali_miti.descriptive}"
+      output_txt = "[#{converted_nepali_miti} BS] #{converted_nepali_miti.descriptive}"
     rescue ConversionUnavailableError => e
       output_txt = e
       @output_color = :red
@@ -35,7 +35,7 @@ module Miti
     desc "to NEPALI_DATE", "converts nepali miti to english date"
     def to_ad(nepali_date)
       converted_english_date = Miti.to_ad(nepali_date)
-      output_txt = "[#{converted_english_date}] #{converted_english_date.strftime('%B %d, %Y %A')}"
+      output_txt = "[#{converted_english_date} AD] #{converted_english_date.strftime('%B %d, %Y %A')}"
     rescue ConversionUnavailableError => e
       output_txt = e
       @output_color = :red
