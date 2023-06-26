850d80f5b03cab0737f64beb33c7341d48615e59
sanzay_mdr <sanzaymanandhar99@gmail.com>
2023-06-26 23:50:29 +0545

    feat: output both nepali miti and english date for cli Miti#today

--- lib/cli.rb
+++ lib/cli.rb
@@ -15,10 +15,12 @@ module Miti
 
     desc "today", "today's nepali miti"
     def today
-      current_nepali_miti = Miti.to_bs(Date.today.to_s)
-      output_txt = "[#{current_nepali_miti} BS] #{current_nepali_miti.descriptive}"
+      date = Date.today
+      current_nepali_miti = Miti.to_bs(date.to_s)
+      formatted_miti = "[#{current_nepali_miti} BS] #{current_nepali_miti.descriptive}"
+      formatted_date = "[#{date} AD] " + date.strftime("%B %d, %Y %A")
 
-      @shell.say(output_txt, :green)
+      @shell.say("#{formatted_miti}\n#{formatted_date}", :green)
     end
 
     desc "on ENGLISH_DATE", "converts english date to nepali miti"
@@ -35,7 +37,7 @@ module Miti
     desc "to NEPALI_DATE", "converts nepali miti to english date"
     def to_ad(nepali_date)
       converted_english_date = Miti.to_ad(nepali_date)
-      output_txt = "[#{converted_english_date} AD] #{converted_english_date.strftime('%B %d, %Y %A')}"
+      output_txt = "[#{converted_english_date} AD] #{converted_english_date.strftime("%B %d, %Y %A")}"
     rescue ConversionUnavailableError => e
       output_txt = e
       @output_color = :red
