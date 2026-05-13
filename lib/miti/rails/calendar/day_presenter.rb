e42735b2f17d27bfd9b3db5a386cebe4f89a34e8
xkshitizx <kshitizlama03@gmail.com>
2026-05-13 14:20:11 +0545

    Add DayPresenter for calendar view helper

---
+++ lib/miti/rails/calendar/day_presenter.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Miti
+  module Rails
+    module Calendar
+      class DayPresenter
+        attr_reader :date
+
+        delegate :gatey, :barsa, :mahina, :bar, :tarik, :to_s, :descriptive, to: :date
+
+        def initialize(date, today:)
+          @date = date
+          @today = today
+        end
+
+        def today?
+          @date.tarik == @today
+        end
+
+        def to_param
+          @date.to_s
+        end
+
+        def ad_date
+          @date.tarik
+        end
+
+        def sunday?
+          @date.bar.zero?
+        end
+
+        def saturday?
+          @date.bar == 6
+        end
+      end
+    end
+  end
+end
